35f5e49a949c2704dcc104cbc874ec0d97d02e02
bandrya77-lang <bandrya77@gmail.com>
2026-03-04 23:11:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ client.on('interactionCreate', async (interaction) => {
 
   const embed = new EmbedBuilder()
     .setTitle("📜 القوانين")
-    .setDescription("اختر من القائمة بالأسفل")
+   .setDescription("⚠️ **نحن غير مسؤولين لعدم قراءتك للقوانين، وستتحمل العواقب كاملة.**")
     .setImage("https://cdn.discordapp.com/attachments/1288579286600515626/1478807328835768432/3fa07620-f1d2-44fb-92c6-b9f4a811b402.jpg?ex=69a9be26&is=69a86ca6&hm=58074c1fba1257b6934a9f9f43cee6e57e54fa1c9d563d54f07c790cd6ec6241&")
     .setColor("#2b2d31");
 
