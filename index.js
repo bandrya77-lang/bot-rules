01b7ebd583a3cfddf6dc2c3043c695027a655396
bandrya77-lang <bandrya77@gmail.com>
2026-03-04 18:23:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,87 +1,74 @@
-const { 
-  Client, 
-  GatewayIntentBits, 
-  REST, 
-  Routes, 
-  ActionRowBuilder, 
-  StringSelectMenuBuilder 
-} = require('discord.js');
+const Discord = require("discord.js")
+const { Client, GatewayIntentBits, DiscordAPIError, ChannelType, CategoryChannel, PermissionsBitField, ApplicationCommandOptionType } = require('discord.js');
+const config = require("./config.json")
+const selecttest = require("./rules.json")
 
-require('dotenv').config();
-
-const config = require("./config.json");
-const selecttest = require("./rules.json");
-
+const { REST, Routes } = require('discord.js');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, Events, ModalBuilder, MessageEmbed, MessageAttachment, StringSelectMenuBuilder, EmbedBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
+const { MessageActionRow, MessageButton } = require('discord.js');
 const client = new Client({
   intents: [
-    GatewayIntentBits.Guilds
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.DirectMessageReactions
   ],
 });
-
-client.once('ready', () => {
+require('dotenv').config()
+client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-});
-
+})
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
   try {
-    await rest.put(
-      Routes.applicationCommands(config.ID_Bot_Discord),
-      {
-        body: [
-          {
-            name: "rules",
-            description: config.cmd_description,
-          },
-        ],
-      }
-    );
-    console.log("Slash command registered.");
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands(config.ID_Bot_Discord), {
+      body: [
+
+        {
+          name: config.cmdRules,
+          description: config.cmd_description,
+        },
+      ]
+    });
+
+    console.log('Successfully reloaded application (/) commands.');
   } catch (error) {
     console.error(error);
   }
 })();
 
-client.on('interactionCreate', async interaction => {
+client.on('interactionCreate', async (interaction) => {
+  if (interaction.commandName === 'rules') {
 
-  if (interaction.isChatInputCommand()) {
-    if (interaction.commandName === "rules") {
 
-      const row = new ActionRowBuilder().addComponents(
+    await interaction.deferReply({ ephemeral: false }).catch((err) => null);
+    const row = new ActionRowBuilder()
+      .addComponents(
         new StringSelectMenuBuilder()
           .setCustomId('rules')
           .setPlaceholder(config.cmd_description)
-          .addOptions(selecttest)
+          .addOptions(selecttest.map(e => {
+            return {
+              ...e
+            }
+          })),
       );
-
-      await interaction.reply({
-        content: config.cmdTitle,
-        ephemeral: true,
-        components: [row],
-      });
-    }
+    
+    await interaction.editReply({ content: config.cmdTitle, ephemeral: true, components: [row] })
   }
-
- if (interaction.isStringSelectMenu()) {
-  if (interaction.customId === "rules") {
-
+  if (interaction.isStringSelectMenu()) {
     const choice = interaction.values[0];
-    const selectedElement = selecttest.find(e => e.value === choice);
+    const selectedElement = selecttest.find(element => choice === element.value);
 
     if (selectedElement) {
-
-      await interaction.deferUpdate();
-
-      await interaction.followUp({
-        content: `${selectedElement.emoji} ${selectedElement.label}\n${selectedElement.message}`,
-        ephemeral: true
-      });
-
+      await interaction.deferReply({ ephemeral: true });
+      await interaction.editReply({ content: `${selectedElement.emoji} ${selectedElement.label} \n ${selectedElement.message}`, ephemeral: true });
     }
   }
-}
-
 });
-
 client.login(process.env.TOKEN);
