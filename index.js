22217c1be010954320268b36b154fd8ef97064c4
bandrya77-lang <bandrya77@gmail.com>
2026-03-04 22:06:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,15 @@ client.on('interactionCreate', async (interaction) => {
           })),
       );
     
-    await interaction.editReply({ content: config.cmdTitle, ephemeral: true, components: [row] })
+   const embed = new EmbedBuilder()
+  .setTitle(`${selectedElement.emoji} ${selectedElement.label}`)
+  .setDescription(selectedElement.message)
+  .setImage("https://cdn.discordapp.com/attachments/1288579286600515626/1478807328835768432/3fa07620-f1d2-44fb-92c6-b9f4a811b402.jpg?ex=69a9be26&is=69a86ca6&hm=58074c1fba1257b6934a9f9f43cee6e57e54fa1c9d563d54f07c790cd6ec6241&")
+  .setColor("#2b2d31");
+
+await interaction.editReply({
+  embeds: [embed]
+});
   }
   if (interaction.isStringSelectMenu()) {
     const choice = interaction.values[0];
@@ -70,7 +78,7 @@ client.on('interactionCreate', async (interaction) => {
  const embed = new EmbedBuilder()
   .setTitle(`${selectedElement.emoji} ${selectedElement.label}`)
   .setDescription(selectedElement.message)
-  .setImage("https://cdn.discordapp.com/attachments/1288579286600515626/1478807328835768432/3fa07620-f1d2-44fb-92c6-b9f4a811b402.jpg?ex=69a9be26&is=69a86ca6&hm=58074c1fba1257b6934a9f9f43cee6e57e54fa1c9d563d54f07c790cd6ec6241&")
+  .setImage("selectedElement.image")
   .setColor("#2b2d31");
 
 await interaction.editReply({
